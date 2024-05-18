5b3622e8035959837b5ceb83ff3d6b54b8a6488b
Flavio-JS <flavim.filho007@gmail.com>
2024-05-18 17:56:47 -0300

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone",
+  compiler: {
+    styledComponents: true,
+  },
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
